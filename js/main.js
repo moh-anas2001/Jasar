cdbb0d671297e095bb86943d8ef378b44b5b95d3
moh-anas2001 <mohamedanascse@gmail.com>
2023-10-23 11:09:00 +0530

    as

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@
     
     var cfg = {
         scrollDuration : 800, // smoothscroll duration
-        mailChimpURL   : 'https://facebook.us8.list-manage.com/subscribe/post?u=cdb7b577e41181934ed6a6a44&amp;id=e6957d85dc'   // mailchimp url
+        mailChimpURL   : "https://gmail.us21.list-manage.com/subscribe/post?u=0868ef99e90760b6b9886836e&amp;id=e2ae6c3a5c&amp;f_id=0002ebe6f0"   // mailchimp url
     },
 
     $WIN = $(window);
