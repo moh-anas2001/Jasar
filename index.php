6bee0b999d89166f7860eecc1c3c35b3ce0bbc5a
asifmuh <muhammedasifsalim2000@gmail.com>
2023-11-21 07:11:49 +0530

    up

--- index.php
+++ index.php
@@ -23,6 +23,7 @@
     <link rel="stylesheet" href="css/base.css">
     <link rel="stylesheet" href="css/vendor.css">
     <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/theme.css">
     <!-- <link rel="stylesheet" href="css/bootstrap.min.css"> -->
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"> -->
 
@@ -39,18 +40,6 @@
     <link rel="icon" href="favicon.ico" type="image/x-icon">
 
 </head>
-
-
-
-
-<style>
- 
-
-
-/*   End media query   */
-
-
-</style>
 
 
 
@@ -1148,7 +1137,7 @@
 
     <!-- footer
     ================================================== -->
-    <footer style="background-color: white;">
+    <!-- <footer style="background-color: white;">
 
         <div class="footer-main">
 
@@ -1165,7 +1154,7 @@
 
 
 
-        </div> <!-- end footer-main -->
+        </div> 
 
         <div class="row footer-bottom">
 
@@ -1179,9 +1168,62 @@
                 </div>
             </div>
 
+        </div> 
+
+    </footer>  -->
+
+
+
+
+    <footer>
+
+        <div class="row footer-main">
+
+            <div class="col-six tab-full left footer-desc">
+
+                <div class="footer-logo"></div>
+                Proin eget tortor risus. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Nulla porttitor accumsan tincidunt. Nulla porttitor accumsan tincidunt. Quaerat voluptas autem necessitatibus vitae aut.
+
+            </div>
+
+            <div class="col-six tab-full right footer-subscribe">
+
+                <h4>Get Notified</h4>
+                <p>Quia quo qui sed odit. Quaerat voluptas autem necessitatibus vitae aut non alias sed quia. Ut itaque enim optio ut excepturi deserunt iusto porro.</p>
+
+                <div class="subscribe-form">
+                    <form id="mc-form" class="group" novalidate="true">
+                        <input type="email" value="" name="EMAIL" class="email" id="mc-email" placeholder="Email Address" required="">
+                        <input type="submit" name="subscribe" value="Subscribe">
+                        <label for="mc-email" class="subscribe-message"></label>
+                    </form>
+                </div>
+
+            </div>
+
+        </div> <!-- end footer-main -->
+
+        <div class="row footer-bottom">
+
+            <div class="col-twelve">
+                <div class="copyright">
+                    <span>© Copyright Glint 2017</span> 
+                    <span>Site Template by <a href="https://www.colorlib.com/">Colorlib</a></span>	
+                </div>
+
+                <div class="go-top">
+                    <a class="smoothscroll" title="Back to Top" href="#top"><i class="icon-arrow-up" aria-hidden="true"></i></a>
+                </div>
+            </div>
+
         </div> <!-- end footer-bottom -->
 
     </footer> <!-- end footer -->
+
+
+
+
+    
 
 
 
