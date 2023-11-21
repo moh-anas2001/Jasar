60357e199e906c9c072e0f00cd51c9ee6032f9e6
asifmuh <muhammedasifsalim2000@gmail.com>
2023-11-21 12:24:09 +0530

    upo

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
     <link rel="stylesheet" href="css/base.css">
     <link rel="stylesheet" href="css/vendor.css">
     <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="css/theme.css">
+    <!-- <link rel="stylesheet" href="css/theme.css"> -->
     <!-- <link rel="stylesheet" href="css/bootstrap.min.css"> -->
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"> -->
 
