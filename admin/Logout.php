7082850270a0089f0d86d415bff48771dc0be07e
moh-anas2001 <mohamedanascse@gmail.com>
2023-11-21 14:00:25 +0400

    12

--- admin/Logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-    session_start();
-    session_destroy();
-    header("Location: index.php");
-    set_message("Logged out successfully");
-?>
--- admin/includes/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$connect = mysqli_connect('localhost', 'jasar', 'secret', 'jasar');
-
-if (mysqli_connect_errno()) {
-    exit('Failed to connect to MySQL :' . mysqli_connect_errno());
-}
-?>
